707cff7f3d74a1e49164d5fd427e8c1ec0303dae
Andres Gutierrez <agutierrez.er@gmail.com>
2020-01-08 18:03:46 +0200

    Changed isPrime function to a more efficient one (Sieve of Eratosthenes)

--- 007/007.js
+++ 007/007.js
@@ -1,25 +1,31 @@
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 // What is the 10 001st prime number?
 
-let primeNumbers = [],
-    number = 2;
+let count = 1,
+    number = 1;
+
 
 function isPrime(n) {
-  let x = Math.floor(Math.sqrt(n));
-
-  for(let i = x; i >= 2; i--) {
-    if(n % i === 0) {
-      return false;
+  if (n === 1) { return false }
+  else if (n < 4) { return true }
+  else if (n % 2 === 0) {return false}
+  else if (n < 9) {return true}
+  else if (n % 3 === 0) {return false}
+  else {
+    r = Math.floor(Math.sqrt(n));
+    f = 5;
+    while(f <= r) {
+      if(n % f === 0) {return false}
+      if(n % (f + 2) === 0) {return false}
+      f = f + 6;
     }
+    return true;
   }
-  return true;
 }
 
-while(primeNumbers.length < 10001) {
-  if(isPrime(number)) {
-    primeNumbers.push(number);
-  }
-  number++;
+while(count < 10001) {
+  if (isPrime(number)) {count++;}
+  number++
 }
 
-console.log(primeNumbers[primeNumbers.length - 1]);
+console.log(number);
--- 010/010.js
+++ 010/010.js
@@ -6,14 +6,21 @@ let num = 2e+6,
     primes = [];
 
 function isPrime(n) {
-  let x = Math.floor(Math.sqrt(n));
-
-  for(let i = x; i >= 2; i--) {
-    if(n % i === 0) {
-      return false;
+  if (n === 1) { return false }
+  else if (n < 4) { return true }
+  else if (n % 2 === 0) {return false}
+  else if (n < 9) {return true}
+  else if (n % 3 === 0) {return false}
+  else {
+    r = Math.floor(Math.sqrt(n));
+    f = 5;
+    while(f <= r) {
+      if(n % f === 0) {return false}
+      if(n % (f + 2) === 0) {return false}
+      f = f + 6;
     }
+    return true;
   }
-  return true;
 }
 
 while(num >= 2) {
